19fff4fa363775b27cd28ff752dc60c69d0ecfa2
P.Kozlowski <pabo@wp.pl>
2023-09-29 03:05:17 +0200

    Main page sketch layout

---
+++ www/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang=pl>
+<head>
+    <meta charset="utf-8"/>
+    <link rel="stylesheet" href="../css/fontello.css"/>
+    <link rel="stylesheet" href="../css/main.css"/>
+    <link rel="stylesheet" href="../css/pages.css"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+<body>
+<header>
+    <div class="header-container">
+        <div class="header-logo"><i class="icon-share"></i>Mose</div>
+        <div></div>
+        <div class="header-ico"><i class="icon-chart-bar"></i></div>
+        <div class="header-ico"><i class="icon-id-card"></i></div>
+    </div>
+</header>
+<main>
+    <div class="content-container">
+        <div>
+            <figure class="page-tile">
+                <div class="page-tile-ico"><img src=""/></div>
+                <figcaption class="page-tile-name">Name</figcaption>
+            </figure>
+            <div class="expand-button" id="expand-button-1"><i class="icon-down-open"></i></div>
+
+            <figure class="page-tile">
+                <div class="page-tile-ico"><img src=""/></div>
+                <figcaption class="page-tile-name">Name</figcaption>
+            </figure>
+            <div class="expand-button" id="expand-button-2"><i class="icon-down-open"></i></div>
+
+            <figure class="page-tile">
+                <div class="page-tile-ico"><img src=""/></div>
+                <figcaption class="page-tile-name">Name</figcaption>
+            </figure>
+            <div class="expand-button" id="expand-button-2"><i class="icon-down-open"></i></div>
+            <figure class="page-tile">
+                <div class="page-tile-ico"><img src=""/></div>
+                <figcaption class="page-tile-name">Name</figcaption>
+            </figure>
+            <div class="expand-button" id="expand-button-2"><i class="icon-down-open"></i></div>
+            <figure class="page-tile">
+                <div class="page-tile-ico"><img src=""/></div>
+                <figcaption class="page-tile-name">Name</figcaption>
+            </figure>
+            <div class="expand-button" id="expand-button-2"><i class="icon-down-open"></i></div>
+        </div>
+
+        <article>
+            y
+        </article>
+    </div>
+</main>
+</header>
+</body>
+</html>
